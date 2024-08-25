090528eee4575146948598adc75b84a3a8fe6f44
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2024-08-25 16:43:12 -0400

    initial portfolio

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./views/index.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+        log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "index", nil)
+	if err != nil {
+        log.Print(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	port := ":5173"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", getIndex)
+
+	log.Printf("starting server in port %s", port)
+	log.Fatal(http.ListenAndServe(port, mux))
+}
