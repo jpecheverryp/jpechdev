526dfc3d78b18f4dc30363dd991995c2e7bc50bf
Juan P Echeverry <70663853+jpecheverryp@users.noreply.github.com>
2025-03-05 22:02:32 -0500

    removed old templating helper functions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
 )
 
 type application struct {
-	logger        *slog.Logger
-	templateCache map[string]*template.Template
+	logger *slog.Logger
 }
 
 func main() {
@@ -17,19 +15,12 @@ func main() {
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	templateCache, err := newTemplateCache()
-	if err != nil {
-		logger.Error(err.Error())
-		os.Exit(1)
-	}
-
 	app := &application{
-		logger:        logger,
-		templateCache: templateCache,
+		logger: logger,
 	}
 
 	logger.Info("starting server", "port", port)
-	err = http.ListenAndServe(port, app.routes())
+	err := http.ListenAndServe(port, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,64 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io/fs"
 	"net/http"
-	"path/filepath"
 
 	"github.com/a-h/templ"
-	"jpech.dev/views"
 )
-
-func newTemplateCache() (map[string]*template.Template, error) {
-	cache := map[string]*template.Template{}
-
-	pages, err := fs.Glob(views.Files, "html/pages/*.html")
-	if err != nil {
-		return nil, err
-	}
-
-	for _, page := range pages {
-		name := filepath.Base(page)
-
-		patterns := []string{
-			"html/layout.html",
-			page,
-		}
-
-		ts, err := template.New(name).ParseFS(views.Files, patterns...)
-		if err != nil {
-			return nil, err
-		}
-
-		cache[name] = ts
-	}
-
-	return cache, nil
-}
 
 func (app *application) renderTempl(w http.ResponseWriter, r *http.Request, status int, component templ.Component) {
 	w.WriteHeader(status)
 	err := templ.Component.Render(component, r.Context(), w)
-	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-}
-
-func (app *application) render(w http.ResponseWriter, status int, page string) {
-	ts, ok := app.templateCache[page]
-	if !ok {
-		err := fmt.Errorf("the template %s does not exist", page)
-		app.logger.Error(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(status)
-
-	err := ts.ExecuteTemplate(w, "layout", nil)
 	if err != nil {
 		app.logger.Error(err.Error())
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
